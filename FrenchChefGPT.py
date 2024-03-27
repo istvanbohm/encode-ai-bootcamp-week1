f8423e3b161a379849d5feb7c0c0023b48c7fe72
Arkano.eth <62271657+arkanoeth@users.noreply.github.com>
2024-03-27 19:47:12 +0100

    Added dotenv functionality

--- FrenchChefGPT.py
+++ FrenchChefGPT.py
@@ -1,6 +1,10 @@
+import os
+from dotenv import load_dotenv
 from openai import OpenAI
 
-client = OpenAI()
+# Load environment variables from .env file
+load_dotenv()
+client = OpenAI( api_key=os.environ.get("OPENAI_API_KEY"),)
 
 messages = [
      {
